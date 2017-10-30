ba15fd4f191a1a26bb9ab803478447445fb3a198
ywang <ywang-prestataire@ina.fr>
2017-10-30 17:26:36 +0100

    sql for client list

--- src/main/java/fr/textma/repository/TeClientDao.java
+++ src/main/java/fr/textma/repository/TeClientDao.java
@@ -4,7 +4,6 @@ package fr.textma.repository;
 import fr.textma.model.TeClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -13,4 +12,8 @@ public interface TeClientDao extends CrudRepository<TeClient, Integer> {
     Page<TeClient> findById(Integer id, Pageable pageable);
     Page<TeClient> findByNameLike(String name, Pageable pageable);
 
+    /*select * from te_client_cli client left join
+    (SELECT fac_cli_id, sum(fac_totalttc) facture_total, count(*) facture_number FROM textma.te_facture_fac where YEAR(fac_datecreation) = YEAR(CURDATE()) group by fac_cli_id) fac
+    on client.cli_id = fac.fac_cli_id order by fac.facture_total desc;
+    */
 }
