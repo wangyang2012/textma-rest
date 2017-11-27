0117cb040bf46b6537cfee0442d694ff22e7c613
wangyang2012 <wangyang1712@gmail.com>
2017-11-27 02:45:03 +0100

    add famille client

--- src/main/java/fr/textma/service/TeFamilleClientServiceImpl.java
+++ src/main/java/fr/textma/service/TeFamilleClientServiceImpl.java
@@ -1,9 +1,10 @@
 package fr.textma.service;
 
-import fr.textma.model.TeClient;
 import fr.textma.model.TeFacture;
+import fr.textma.model.TeFamilleClient;
 import fr.textma.repository.TeClientDao;
 import fr.textma.repository.TeFactureDao;
+import fr.textma.repository.TeFamilleClientDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,18 +12,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-@Service("teFactureService")
+@Service("teFamilleClientService")
 @Transactional
-public class TeFactureServiceImpl implements TeFactureService{
+public class TeFamilleClientServiceImpl implements TeFamilleClientService{
 
 	@Autowired
-	private TeFactureDao dao;
+	private TeFamilleClientDao dao;
 
-	@Autowired
-	private TeClientDao clientDao;
 
 	@Override
-	public Page<TeFacture> findByClientId(Integer clientId, Pageable pageable) {
-		return dao.findCurrentYearFacturesByClientId(clientId, pageable);
+	public Page<TeFamilleClient> findByNomLike(String nom, Pageable pageable) {
+		return dao.findByNomLike("%"+nom+"%", pageable);
 	}
 }
---
+++ src/main/java/fr/textma/web/rest/client/TeFamilleClientRestSource.java
@@ -0,0 +1,59 @@
+package fr.textma.web.rest.client;
+
+import fr.textma.model.TeContact;
+import fr.textma.model.TeFamilleClient;
+import fr.textma.model.WebixDatatableResponse;
+import fr.textma.service.TeContactService;
+import fr.textma.service.TeFamilleClientService;
+import liquibase.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/textma")
+@CrossOrigin(origins = "*")
+public class TeFamilleClientRestSource {
+
+    @Autowired
+    TeFamilleClientService service;
+
+    @GetMapping(value = "/teFamilleClients")
+    public WebixDatatableResponse<TeFamilleClient> listTeFamilleClients(@RequestParam(defaultValue = "", required = false) String search, @RequestParam(defaultValue = "20", required = false) Integer count, @RequestParam(defaultValue = "0", required = false) Integer start, @RequestParam(required = false) Map<String, String> filter) {
+        Integer page = start / count;
+        Sort sort = getSortInfo(filter);
+        Pageable pageable = new PageRequest(page, count, sort);
+        Page<TeFamilleClient> familleClients = service.findByNomLike(search, pageable);
+        return new WebixDatatableResponse<TeFamilleClient>(familleClients, start);
+    }
+
+    private Sort getSortInfo(@RequestParam(required = false) Map<String, String> filter) {
+
+        String id = filter.get("sort[id]");
+        String nom = filter.get("sort[nom]");
+
+        Sort sort = null;
+        if (!StringUtils.isEmpty(id)) {
+            if ("asc".equals(id)) {
+                sort = new Sort(Sort.Direction.ASC, "id");
+            } else {
+                sort = new Sort(Sort.Direction.DESC, "id");
+            }
+        } else if (!StringUtils.isEmpty(nom)) {
+            if ("asc".equals(nom)) {
+                sort = new Sort(Sort.Direction.ASC, "nom");
+            } else {
+                sort = new Sort(Sort.Direction.DESC, "nom");
+            }
+        } else {
+            sort = new Sort(Sort.Direction.ASC, "id");
+        }
+        return sort;
+    }
+}
