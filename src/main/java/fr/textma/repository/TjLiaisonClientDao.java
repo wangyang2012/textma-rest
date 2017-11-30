225a410d3e7f4da1f061fc965b63514d46125f74
yang <wangyang1712@gmail.com>
2017-11-30 23:27:03 +0100

    fix search of principaux clients

--- src/main/java/fr/textma/repository/TjLiaisonClientDao.java
+++ src/main/java/fr/textma/repository/TjLiaisonClientDao.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 
 public interface TjLiaisonClientDao extends CrudRepository<TjLiaisonClient, Integer> {
     TjLiaisonClient findOneById(Integer id);
@@ -16,6 +18,6 @@ public interface TjLiaisonClientDao extends CrudRepository<TjLiaisonClient, Inte
     Page<TjLiaisonClient> findByClientMere(TeClient clientMere, Pageable pageable);
     @Query("select teClient from TeClient teClient where teClient.id in (select distinct clientMere.id from TjLiaisonClient)")
     Page<TeClient> findAllClientMere(Pageable pageable);
-    @Query("select teClient from TeClient teClient where teClient.id in (select distinct clientMere.id from TjLiaisonClient) and (teClient.nom like '%search%' or teClient.adresse like '%search%' or teClient.ville like '%search%')")
+    @Query("select teClient from TeClient teClient where teClient.id in (select distinct clientMere.id from TjLiaisonClient) and (teClient.nom like %:search% or teClient.adresse like %:search% or teClient.ville like %:search%)")
     Page<TeClient> findAllClientMereWithSearch(@Param("search") String search, Pageable pageable);
 }
