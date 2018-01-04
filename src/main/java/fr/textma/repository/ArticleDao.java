972c34c23fee862adc36c5d739098e2dee551d4b
ywang <ywang-prestataire@ina.fr>
2018-01-04 15:28:27 +0100

    remove "public" in interface

--- src/main/java/fr/textma/repository/ArticleDao.java
+++ src/main/java/fr/textma/repository/ArticleDao.java
@@ -4,7 +4,6 @@ package fr.textma.repository;
 import fr.textma.model.Article;
 import fr.textma.model.CollectionArticle;
 import fr.textma.model.GammeArticle;
-import fr.textma.model.RemiseBrut;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -12,5 +11,5 @@ import java.util.List;
 
 public interface ArticleDao extends CrudRepository<Article, Integer> {
 
-    public List<Article> findByCollectionAndGamme(CollectionArticle collection, GammeArticle gamme);
+    List<Article> findByCollectionAndGamme(CollectionArticle collection, GammeArticle gamme);
 }
